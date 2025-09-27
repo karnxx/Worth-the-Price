f463ff79e9b17393bc83485b05a5dc3eb2e376b5
Fastestkyo <fastestkyo@gmail.com>
2025-09-27 09:28:07 +0530

    hjk

--- scene/char/plr/plr.gd
+++ scene/char/plr/plr.gd
@@ -1,50 +1,68 @@
-extends CharacterBody2D
+extends CharacterBody3D
+var current_health
+var current_stm
+var current_dmg
+var current_roll_dis
+@export var roll_dis : int
+@export var health :int
+@export var stm:int
+@export var dmg:int
 
-@export var speed: float = 200.0
+@export var speed: float = 2.0
+#	-health ui
+#	-stm ui
+#	-ability to roll
+#	-colours
+#	-vision-blur-viginette
+#	-losing allies
+#	-enemy healthbar
+#	-ability to move in a dir-only 1 round
+#	-dmg lag
+#	-roll i frames
+#	-Sound
+#	-delayed controls
+#	-speed
+var input_dir: Vector2
+var move_dir: Vector3
 
 func _physics_process(delta: float) -> void:
-	var dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = dir.normalized() * speed
-	
-	if dir != Vector2.ZERO:
-		walkanim(dir)
+	input_dir = Input.get_vector("ui_left", "ui_right", "ui_up","ui_down")
+	move_dir = Vector3(input_dir.x, 0, input_dir.y)
+	if move_dir != Vector3.ZERO:
+		move_dir = move_dir.normalized() * speed
+		velocity.x = -move_dir.x
+		velocity.z = -move_dir.z
+		walkanim(input_dir)
 	else:
-		idleanim(dir)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+		idleanim(input_dir)
+	if !is_on_floor():
+		velocity.y -= 10 * 10
+	
 	
 	move_and_slide()
 
-func walkanim(dir) -> void:
-	if dir.x > 0 and dir.y == 0:
-		$animate.play("walk_right")
-	elif dir.x < 0 and dir.y == 0:
-		$animate.play("walk_left")
+func walkanim(dir=) -> void:
+	if dir.x < 0 and dir.y == 0:
+		$AnimatedSprite3D.play("walk_right")
+	elif dir.x > 0 and dir.y == 0:
+		$AnimatedSprite3D.play("walk_left")
 	elif dir.y < 0 and dir.x == 0:
-		$animate.play("walk_up")
+		$AnimatedSprite3D.play("walk_up")
 	elif dir.y > 0 and dir.x == 0:
-		$animate.play("walk_down")
-	elif dir.x > 0 and dir.y < 0:
-		$animate.play("walk_topright")
+		$AnimatedSprite3D.play("walk_down")
 	elif dir.x < 0 and dir.y < 0:
-		$animate.play("walk_topleft")
-	elif dir.x > 0 and dir.y > 0:
-		$animate.play("wawlk_bottomright")
+		$AnimatedSprite3D.play("walk_topright")
+	elif dir.x > 0 and dir.y < 0:
+		$AnimatedSprite3D.play("walk_topleft")
 	elif dir.x < 0 and dir.y > 0:
-		$animate.play("walk_bottomleft")
+		$AnimatedSprite3D.play("wawlk_bottomright")
+	elif dir.x > 0 and dir.y > 0:
+		$AnimatedSprite3D.play("walk_bottomleft")
 
 func idleanim(dir) -> void:
-	if dir.x > 0 and dir.y == 0:
-		$animate.play("walk_right")
-	elif dir.x < 0 and dir.y == 0:
-		$animate.play("walk_left")
-	elif dir.y < 0 and dir.x == 0:
-		$animate.play("walk_up")
-	elif dir.y > 0 and dir.x == 0:
-		$animate.play("walk_down")
-	elif dir.x > 0 and dir.y < 0:
-		$animate.play("walk_topright")
-	elif dir.x < 0 and dir.y < 0:
-		$animate.play("walk_topleft")
-	elif dir.x > 0 and dir.y > 0:
-		$animate.play("wawlk_bottomright")
-	elif dir.x < 0 and dir.y > 0:
-		$animate.play("walk_bottomleft")
+	if dir == Vector2.ZERO:
+		print('ad')
+	else:
+		walkanim(dir)
