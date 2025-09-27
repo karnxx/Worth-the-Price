d0319458a96be09501b3d7b2be7a9b33a6533442
Fastestkyo <fastestkyo@gmail.com>
2025-09-27 08:15:47 +0530

    asf

--- scene/char/plr/plr.gd
+++ scene/char/plr/plr.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+func _physics_process(delta: float) -> void:
+	movement()
+	move_and_slide()
 
 func movement():
 	var dir= Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
