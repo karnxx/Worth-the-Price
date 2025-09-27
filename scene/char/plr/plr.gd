eb7876cc3dad2491b18fdfb8cf73ff4014c0b638
Fastestkyo <fastestkyo@gmail.com>
2025-09-27 08:21:38 +0530

    ads

--- scene/char/plr/plr.gd
+++ scene/char/plr/plr.gd
@@ -5,9 +5,28 @@ extends CharacterBody2D
 func _physics_process(delta: float) -> void:
 	var dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = dir.normalized() * speed
-	if dir.x > 0:
-		naimat(walk_right)
+	
+	if dir != Vector2.ZERO:
+		walkanim(dir)
+	else:
+		$animate.play("idle")
+	
 	move_and_slide()
 
-func naimat(animation):
-	$animate.play(animation)
+func walkanim(dir) -> void:
+	if dir.x > 0 and dir.y == 0:
+		$animate.play("walk_right")
+	elif dir.x < 0 and dir.y == 0:
+		$animate.play("walk_left")
+	elif dir.y < 0 and dir.x == 0:
+		$animate.play("walk_up")
+	elif dir.y > 0 and dir.x == 0:
+		$animate.play("walk_down")
+	elif dir.x > 0 and dir.y < 0:
+		$animate.play("walk_topright")
+	elif dir.x < 0 and dir.y < 0:
+		$animate.play("walk_topleft")
+	elif dir.x > 0 and dir.y > 0:
+		$animate.play("wawlk_bottomright")
+	elif dir.x < 0 and dir.y > 0:
+		$animate.play("walk_bottomleft")
